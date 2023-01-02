534e3188833026a671ed288a23863c782e80c691
¨jeronimoGarcia¨ <jero_garcia23@hotmail.com>
2023-01-02 16:48:36 +0100

    Slideshow de imagenes

--- pages/product/slug.tsx
+++ pages/product/slug.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Chip, Grid, Typography } from "@mui/material";
 import React from "react";
 import { ShopLayout } from "../../components/layouts";
+import { ProductSlideshow } from "../../components/products";
 import { initialData } from "../../database/products";
 
 const product = initialData.products[0];
@@ -9,8 +10,9 @@ const ProductPage = () => {
   return (
     <ShopLayout title={product.title} pageDescription={product.description}>
       <Grid container spacing={3}>
-        <Grid item xs={12} sm={7}></Grid>
-
+        <Grid item xs={12} sm={7}>
+          <ProductSlideshow images={product.images} />
+        </Grid>
         <Grid item xs={12} sm={5}>
           <Box display="flex" flexDirection="column">
             <Typography variant="h1" component="h1">
@@ -19,15 +21,17 @@ const ProductPage = () => {
             <Typography variant="subtitle1" component="h2">
               ${product.price}
             </Typography>
-            <Box sx={{my:2}}>
+            <Box sx={{ my: 2 }}>
               <Typography variant="subtitle2">Cantidad</Typography>
             </Box>
 
-            <Button color='secondary' className="circular-btn">Agregar al carrito</Button>
+            <Button color="secondary" className="circular-btn">
+              Agregar al carrito
+            </Button>
 
             {/* <Chip label='No hay disponibles' color="error" variant="outlined"></Chip> */}
 
-            <Box sx={{mt:3}}>
+            <Box sx={{ mt: 3 }}>
               <Typography variant="subtitle2">Description</Typography>
               <Typography variant="body2">{product.description}</Typography>
             </Box>
