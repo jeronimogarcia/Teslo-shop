7f4cb86652a5c01c1473c77094a4b3e4af48598e
¨jeronimoGarcia¨ <jero_garcia23@hotmail.com>
2023-01-02 14:29:01 +0100

    ProductList & ProductCard Renderizado en Home

---
+++ components/products/ProductCard.tsx
@@ -0,0 +1,24 @@
+import { Card, CardActionArea, CardMedia, Grid } from "@mui/material";
+import React from "react";
+import { IProduct } from "../../interfaces";
+import { FC } from "react";
+
+interface Props {
+  product: IProduct;
+}
+
+export const ProductCard: FC<Props> = ({ product }) => {
+  return (
+    <Grid item xs={6} sm={4} key={product.slug}>
+      <Card>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            image={`products/${product.images[0]}`}
+            alt={product.title}
+          />
+        </CardActionArea>
+      </Card>
+    </Grid>
+  );
+};
