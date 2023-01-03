32f5feb4916af24192f4d75e1113e79f6e61bf93
¨jeronimoGarcia¨ <jero_garcia23@hotmail.com>
2023-01-03 13:17:58 +0100

    Pagina de pago

---
+++ pages/orders/[id].tsx
@@ -0,0 +1,101 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Chip,
+  Divider,
+  Grid,
+  Link,
+  Typography,
+} from "@mui/material";
+import { CartList, OrderSummary } from "../../components/cart";
+import { ShopLayout } from "../../components/layouts";
+import NextLink from "next/link";
+import {
+  CreditCardOffOutlined,
+  CreditScoreOutlined,
+} from "@mui/icons-material";
+
+const OrderPage = () => {
+  return (
+    <ShopLayout
+      title={"Resumen de la orden: numero de orden"}
+      pageDescription={"Resumen de la orden"}
+    >
+      <Typography variant="h1" component="h1">
+        Order: numero de orden
+      </Typography>
+
+      {/* <Chip
+        sx={{ my: 2 }}
+        label="Pendiente de pago"
+        variant="outlined"
+        color="error"
+        icon={<CreditCardOffOutlined />}
+      /> */}
+
+      <Chip
+        sx={{ my: 2 }}
+        label="Orden ya fue pagada"
+        variant="outlined"
+        color="success"
+        icon={<CreditScoreOutlined />}
+      />
+
+      <Grid container>
+        <Grid item xs={12} sm={7}>
+          <CartList />
+        </Grid>
+        <Grid item xs={12} sm={5}>
+          <Card className="summary-card">
+            <CardContent>
+              <Typography variant="h2">Resumen (3 productos)</Typography>
+              <Divider sx={{ my: 1 }} />
+
+              <Box display="flex" justifyContent="space-between">
+                <Typography variant="subtitle1">
+                  Direccion de entrega
+                </Typography>
+                <NextLink href="/checkout/address" passHref legacyBehavior>
+                  <Link underline="always">Editar</Link>
+                </NextLink>
+              </Box>
+
+              <Typography>Jeronimo Garcia</Typography>
+              <Typography>Artigas 376</Typography>
+              <Typography>Rosario, Sta Fe</Typography>
+              <Typography>Argentina</Typography>
+              <Typography>324235523</Typography>
+
+              <Divider sx={{ my: 1 }} />
+
+              <Box display="flex" justifyContent="space-between">
+                <Typography variant="subtitle1">Calculo costo</Typography>
+                <NextLink href="/cart" passHref legacyBehavior>
+                  <Link underline="always">Editar</Link>
+                </NextLink>
+              </Box>
+
+              <OrderSummary />
+
+              <Box sx={{ mt: 3 }}>
+                <h2>Pagar</h2>
+
+                <Chip
+                  sx={{ my: 2 }}
+                  label="Orden ya fue pagada"
+                  variant="outlined"
+                  color="success"
+                  icon={<CreditScoreOutlined />}
+                />
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </ShopLayout>
+  );
+};
+
+export default OrderPage;
