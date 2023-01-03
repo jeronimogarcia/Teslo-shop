66cbb8bb72aac630f650d751833ccd1d6ee928e2
¨jeronimoGarcia¨ <jero_garcia23@hotmail.com>
2023-01-03 09:34:25 +0100

    ItemCounter component

--- pages/product/slug.tsx
+++ pages/product/slug.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Chip, Grid, Typography } from "@mui/material";
 import React from "react";
 import { ShopLayout } from "../../components/layouts";
 import { ProductSlideshow } from "../../components/products";
+import { ItemCounter } from "../../components/ui";
 import { initialData } from "../../database/products";
 
 const product = initialData.products[0];
@@ -23,6 +24,7 @@ const ProductPage = () => {
             </Typography>
             <Box sx={{ my: 2 }}>
               <Typography variant="subtitle2">Cantidad</Typography>
+              <ItemCounter/>
             </Box>
 
             <Button color="secondary" className="circular-btn">
