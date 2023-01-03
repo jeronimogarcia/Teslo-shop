6710ec380c3219faa8363312baa2559ae47dc901
¨jeronimoGarcia¨ <jero_garcia23@hotmail.com>
2023-01-03 09:53:18 +0100

    SizeSelector Component

--- pages/product/slug.tsx
+++ pages/product/slug.tsx
@@ -1,7 +1,7 @@
 import { Box, Button, Chip, Grid, Typography } from "@mui/material";
 import React from "react";
 import { ShopLayout } from "../../components/layouts";
-import { ProductSlideshow } from "../../components/products";
+import { ProductSlideshow, SizeSelector } from "../../components/products";
 import { ItemCounter } from "../../components/ui";
 import { initialData } from "../../database/products";
 
@@ -25,6 +25,7 @@ const ProductPage = () => {
             <Box sx={{ my: 2 }}>
               <Typography variant="subtitle2">Cantidad</Typography>
               <ItemCounter/>
+              <SizeSelector selectedSize={product.sizes[0]} sizes={product.sizes}/>
             </Box>
 
             <Button color="secondary" className="circular-btn">
