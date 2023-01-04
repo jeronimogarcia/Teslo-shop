05f0779bb843a8857f8b4ff4aca9312b51c91f08
¨jeronimoGarcia¨ <jero_garcia23@hotmail.com>
2023-01-04 15:50:38 +0100

    UI Context

---
+++ context/ui/UiProvider.tsx
@@ -0,0 +1,31 @@
+import { FC, useReducer } from 'react';
+import { UiContext, uiReducer } from './';
+
+export interface UiState {
+    isMenuOpen: boolean;
+    children?: React.ReactNode | undefined
+}
+
+
+const UI_INITIAL_STATE: UiState = {
+    isMenuOpen: false,
+}
+
+
+export const UiProvider:FC<UiState> = ({ children }) => {
+
+    const [state, dispatch] = useReducer( uiReducer , UI_INITIAL_STATE );
+
+    const toogleSideMenu = () => {
+        dispatch({ type: '[UI] - ToogleMenu'})
+    }
+
+    return (
+        <UiContext.Provider value={{
+            ...state,
+            toogleSideMenu
+        }}>
+            { children }
+        </UiContext.Provider>
+    )
+};
