792e7af68a4be908df0810ad8f06210f5a06bf69
¨jeronimoGarcia¨ <jero_garcia23@hotmail.com>
2023-01-09 16:26:05 +0100

    Insercion nuevos usuarios. Encriptado de contraseñas. bcryptjs

--- database/index.ts
+++ database/index.ts
@@ -1,5 +1,5 @@
 
 export * as db from './db'
 export * from './constants'
-export * as seedDatabase from './products'
+export * as seedDatabase from './seed-data'
 export * as dbProducts from './dbProducts'
