213c18dd929b67fbe0bc670913cf116499e0c481
¨jeronimoGarcia¨ <jero_garcia23@hotmail.com>
2023-01-10 14:25:14 +0100

    Mostrar mensaje de error en pantalla al intentar logear de forma erronea

--- pages/auth/login.tsx
+++ pages/auth/login.tsx
@@ -1,9 +1,19 @@
-import { Box, Button, Grid, Link, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Chip,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { AuthLayout } from "../../components/layouts";
 import NextLink from "next/link";
 import { useForm } from "react-hook-form";
 import { validations } from "../../utils";
 import { tesloApi } from "../../api";
+import { ErrorOutline } from "@mui/icons-material";
+import { useState } from "react";
 
 type FormData = {
   email: string;
@@ -11,21 +21,28 @@ type FormData = {
 };
 
 const LoginPage = () => {
-  const onLoginUser = async ({ email, password }: FormData) => {
-    try {
-      const { data } = await tesloApi.post("/user/login", { email, password });
-      const { token, user } = data;
-      console.log({ token, user });
-    } catch (error) {
-      console.log("Error en las credenciales");
-    }
-  };
+  const [showError, setShowError] = useState(false);
 
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<FormData>();
+
+  const onLoginUser = async ({ email, password }: FormData) => {
+
+    setShowError(false);
+
+    try {
+      const { data } = await tesloApi.post("/user/login", { email, password });
+      const { token, user } = data;
+      console.log({ token, user });
+    } catch (error) {
+      setShowError(true);
+      console.log("Error en las credenciales");
+      setTimeout(() => setShowError(false), 3000);
+    }
+  };
 
   return (
     <AuthLayout title={"Ingresar"}>
@@ -36,6 +53,13 @@ const LoginPage = () => {
               <Typography variant="h1" component="h1">
                 Iniciar Sesion
               </Typography>
+              <Chip
+                label="No reconocemos ese usuario / contraseña"
+                color="error"
+                icon={<ErrorOutline />}
+                className="fadeIn"
+                sx={{ display: showError ? "flex" : "none" }}
+              />
             </Grid>
 
             <Grid item xs={12}>
@@ -44,9 +68,8 @@ const LoginPage = () => {
                 label="Correo"
                 variant="filled"
                 fullWidth
-                {...(register("email"),
-                {
-                  required: true,
+                {...register("email", {
+                  required: "Este campo es requerido",
                   validate: validations.isEmail,
                 })}
                 error={!!errors.email}
@@ -58,11 +81,12 @@ const LoginPage = () => {
                 label="Password"
                 variant="filled"
                 fullWidth
-                {...(register("password"),
-                {
-                  required: true,
-                  minLength: { value: 6, message: "Minimo 6 caracteres" },
+                {...register("password", {
+                  required: "Este campo es requerido",
+                  minLength: { value: 6, message: "Mínimo 6 caracteres" },
                 })}
+                error={!!errors.password}
+                helperText={errors.password?.message}
               />
             </Grid>
 
