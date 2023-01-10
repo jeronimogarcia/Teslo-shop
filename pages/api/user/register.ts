b731fbb1815b783e0f7d0f8099f4e83bb87fb28b
¨jeronimoGarcia¨ <jero_garcia23@hotmail.com>
2023-01-10 09:52:07 +0100

    Validacion de formato de email

--- pages/api/user/register.ts
+++ pages/api/user/register.ts
@@ -2,18 +2,18 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import bcrypt from "bcryptjs";
 import { db } from "../../../database";
 import { User } from "../../../models";
-import { jwt } from "../../../utils";
+import { jwt, validations } from "../../../utils";
 
 type Data =
-| { message: string }
-| {
-    token: string;
-    user: {
-      email: string;
-      name: string;
-      role: string;
+  | { message: string }
+  | {
+      token: string;
+      user: {
+        email: string;
+        name: string;
+        role: string;
+      };
     };
-};
 
 export default function handler(
   req: NextApiRequest,
@@ -29,60 +29,64 @@ export default function handler(
   }
 }
 
-const registerUser = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  const { email = '', password = '', name = '' } = req.body as { email: string, password: string, name: string};
+const registerUser = async (
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) => {
+  const {
+    email = "",
+    password = "",
+    name = "",
+  } = req.body as { email: string; password: string; name: string };
 
-  if ( password.length < 6) {
-    res.status(400).json({message: 'La contraseña debe tener mas de 6 caracteres'})
+  if (password.length < 6) {
+    res
+      .status(400)
+      .json({ message: "La contraseña debe tener mas de 6 caracteres" });
   }
 
-  if ( name.length < 3) {
-    return res.status(400).json({message: 'El nombre debe tener mas de 3 caracteres'})
+  if (name.length < 3) {
+    return res
+      .status(400)
+      .json({ message: "El nombre debe tener mas de 3 caracteres" });
   }
 
-  // TODO: validar email
+  if (!validations.isValidEmail(email)) {
+    return res.status(400).json({
+      message: "El correo no tiene formato de correo",
+    });
+  }
 
-//   if (user) {
-//     await db.disconnect();
-//     return res
-//       .status(400)
-//       .json({ message: "Correo ya usado - Email" });
-//   }
+  await db.connect();
 
-
-await db.connect();
-
-const user = await User.findOne({ email });
+  const user = await User.findOne({ email });
 
   if (user) {
     await db.disconnect();
-    return res
-      .status(400)
-      .json({ message: "Correo ya usado - Email" });
+    return res.status(400).json({ message: "Correo ya usado - Email" });
   }
-
 
   const newUser = new User({
     email: email.toLocaleLowerCase(),
-    password: bcrypt.hashSync( password ),
-    role: 'client',
+    password: bcrypt.hashSync(password),
+    role: "client",
     name,
-  })
+  });
 
   try {
     await newUser.save({
-        validateBeforeSave: true
-    })
+      validateBeforeSave: true,
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return res.status(500).json({
-        message: 'Revisar logs del servidor'
-    })
+      message: "Revisar logs del servidor",
+    });
   }
 
   const { _id, role } = newUser;
 
-  const token = jwt.signToken(_id, email)
+  const token = jwt.signToken(_id, email);
 
   return res.status(200).json({
     token: token,
